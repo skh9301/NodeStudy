162e49862b05f4559a2ae7d01126eb33ed3dd424
skh9301 <skh9301@gmail.com>
2024-06-17 10:29:15 +0900

    24-06-17 시작

---
+++ NodeClassCh03/my-app01/index01.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const url = require('url');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/main', (req, res) => {
+  const user = url.parse(req.url, true).query;
+  console.log(user);
+  res.json({ name: user.name + '-한글', age: user.age });
+});
+
+app.get('/list', (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+  res.send(`
+    <h1>여기는 리스트</h1>
+    <ul>
+      <li>Node.js</li>
+      <li>JavaScript</li>
+      <li>Node.js</li>
+      <li>Node.js</li>
+    </ul>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Express 서버시작 -ok- http://localhost:${port}`);
+});
